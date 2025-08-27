f40f7183a88223019d9f0bce667dbc84be09854c
Rabeet-Ahmer <rabeetahmer9749@gmail.com>
2025-08-27 09:50:14 +0500

    Update "companion" folder to "companions"

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import { recentSessions } from "@/constants";
+import React from "react";
 
 const Page = () => {
+  const PopularCompanions = [
+    {id: "123", name: "Neura the Brainy Explorer", topic: "Topic: Neural Network of the Brain", subject: "Science", duration: 45, color: "#E5D0FF"},
+    {id: "456", name: "Countsy the Number Wizard", topic: "Topic: Neural Network of the Brain", subject: "Maths", duration: 30, color: "#FFDA6E"},
+    {id: "789", name: "Verba The Vocabulary Builder", topic: "Topic: English Literature ", subject: "Language", duration: 30, color: "#BDE7FF"},
+  ]
   return (
-    <div>Page</div>
-  )
-}
+    <main>
+      <h1>Popular Companions</h1>
 
-export default Page
+      <section className="home-section">
+        {PopularCompanions.map((item) => (
+          <CompanionCard key={item.id} id={item.id} name={item.name} topic={item.topic} subject={item.subject} duration={item.duration} color={item.color}  />
+        ))}
+      </section>
+
+      <section className="home-section">
+        <CompanionsList 
+        companions = {recentSessions}
+        classNames = "w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
+  );
+};
+
+export default Page;
