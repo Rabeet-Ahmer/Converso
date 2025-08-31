c99065b1a44db0a67246c81caf779955ff79c53c
Rabeet-Ahmer <rabeetahmer9749@gmail.com>
2025-08-31 22:21:10 +0500

    Adjust post configuration

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,24 +1,25 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 import Link from "next/link";
 import NavItems from "./NavItems";
-import {
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const NavBar = () => {
   return (
     <nav className="navbar">
       <Link href={"/"}>
         <div className="flex items-center cursor-pointer gap-2.5">
-          <Image src={"/images/logo.svg"} alt="logo" width={46} height={44} />
+          <Image src={"/images/logo.png"} alt="logo" width={50} height={50} />
+          <h1>Mentora</h1>
         </div>
+        <div></div>
       </Link>
-      <div className="flex items-center gap-8">
+      <button className="btn-primary">
+        <Image src={"/icons/plus.svg"} alt="plus" width={12} height={12} />
+        <Link href={"/companions/new"}>Build a new companion</Link>
+      </button>
+      <div className="flex items-center gap-8 -ml-28">
         <NavItems />
         <SignedOut>
           <SignInButton>
@@ -26,7 +27,7 @@ const NavBar = () => {
           </SignInButton>
         </SignedOut>
         <SignedIn>
-            <UserButton />
+          <UserButton />
         </SignedIn>
       </div>
     </nav>
